05b40957409f020a42ede7050ea4930e7dcc7daa
FaideWW <michael@faide.net>
2017-09-23 09:54:11 -0600

    Move to Recharts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import TrinketChart from './components/TrinketChart';
+import trinketJSON from '../2017-8-17-test-collation-4/full.json';
 
+console.log('trinkets', trinketJSON);
 export default class App extends Component {
   render() {
     return (
--- src/components/TrinketChart.jsx
+++ src/components/TrinketChart.jsx
@@ -1,15 +1,29 @@
 import React  from 'react';
 import PropTypes from 'prop-types';
 
-import { Bar } from 'react-chartjs-2';
+import { BarChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar } from 'recharts';
+
+const colors = {
+  900: 'blue',
+  905: 'red',
+};
+const data = [
+  { name: 'Trinket A', '900': 10, '905': 15 },
+  { name: 'Trinket B', '900': 15, '905': 20 },
+];
 
 function TrinketChart(props) {
   return (
     <div>
-      <Bar
-        label={['one', 'two']}
-        data={[10, 20]}
-      />
+    <BarChart layout="vertical" data={data} width={500} height={600}>
+    <YAxis dataKey="name" type="category" />
+    <XAxis type="number"/>
+    <CartesianGrid horizontal={false} />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey="900" stackId="a" fill={colors[900]} />
+    <Bar dataKey="905" stackId="a" fill={colors[905]} />
+    </BarChart>
     </div>
   );
 }
